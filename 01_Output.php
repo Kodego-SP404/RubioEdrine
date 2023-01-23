53338b7b7380963ae527690ee8c40af113ad3d98
edrine13 <edrine.rubio@gmail.com>
2023-01-23 10:43:16 +0800

    My First PHP

---
+++ 01_Output.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+// $name = "Edrine";
+// $age = 31;
+
+// echo "The student's name is $name, $age years old";
+// echo "<br>";
+// echo 123;
+// echo '<h1>Hello World</h1>';
+
+//print - similar to echo, butr a bit slower
+// print 'Hello';
+
+//print_r - Gives a bit more info. Can be used to print arrays
+// print_r('Hello');
+// echo "<br>";
+// print_r([1,2,3]);
+
+//var_dump - More info data type and length
+// var_dump('Hello');
+var_dump([1,2,3]);
+
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
+
